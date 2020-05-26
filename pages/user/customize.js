aa988c7e0be8cdfca6b1db5598a4747e90c04d37
Jonathas Andrade <7339257+eljonathas@users.noreply.github.com>
2020-05-26 11:27:10 -0300

    Update customize.js

--- pages/user/customize.js
+++ pages/user/customize.js
@@ -64,12 +64,17 @@ export default function Customize(){
             readFile.readAsDataURL(file);
             readFile.onload = function(){
                 const readResult = this.result.substr((this.result.search(/base64,/)+7));
+                
+                /**
+                * @params Client-ID need a valid token based on Imgur API
+                * see it on: https://apidocs.imgur.com/ and https://imgur.com
+                */
 
                 axios({
                     method: 'POST',
                     url: 'https://api.imgur.com/3/image',
                     headers: {
-                        Authorization: 'Client-ID 90b04794aac6a13',
+                        Authorization: 'Client-ID YOU-APPLICATION-ID',
                     },
                     data: {
                         image: readResult,
